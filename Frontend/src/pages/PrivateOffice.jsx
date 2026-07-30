c760a2d5746bf15945e0ec973029610ecef70ea4
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-07-30 19:21:21 +0500

    Update PrivateOffice.jsx

--- Frontend/src/pages/PrivateOffice.jsx
+++ Frontend/src/pages/PrivateOffice.jsx
@@ -788,6 +788,28 @@ function PrivateOffice() {
     }
   })}
 </script>
+
+
+<script type="application/ld+json">
+  {JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: "https://www.aegiscoworking.ae/"
+      },
+      {
+       "@type": "ListItem",
+        position: 2,
+        name: "Private Office",
+        item: "https://www.aegiscoworking.ae/private-office"
+      }
+    ]
+  })}
+</script>
       </Helmet>
 
       <Navbar />
