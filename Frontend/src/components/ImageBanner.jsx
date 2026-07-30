3c763a65895b16f0cdcc4a0f7ee9634c8e1461e3
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-07-30 15:23:05 +0500

    Update ImageBanner.jsx

--- Frontend/src/components/ImageBanner.jsx
+++ Frontend/src/components/ImageBanner.jsx
@@ -1,7 +1,26 @@
-function ImageBanner({ image, alt = "Coworking space" }) {
+// function ImageBanner({ image, alt = "Coworking space" }) {
+//   return (
+//     <section className="image-banner">
+//       <img src={image} alt={alt} />
+//     </section>
+//   )
+// }
+
+// export default ImageBanner
+
+
+
+
+
+function ImageBanner({ image, alt = "Coworking space in Abu Dhabi" }) {
   return (
     <section className="image-banner">
-      <img src={image} alt={alt} />
+      <img
+        src={image}
+        alt={alt}
+        loading="lazy"
+        decoding="async"
+      />
     </section>
   )
 }
