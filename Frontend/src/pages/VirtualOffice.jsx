c53b38a27173417c568b3d093b48bb77184c3e9b
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-07-30 19:21:51 +0500

    Update VirtualOffice.jsx

--- Frontend/src/pages/VirtualOffice.jsx
+++ Frontend/src/pages/VirtualOffice.jsx
@@ -475,6 +475,28 @@ function VirtualOffice() {
     }
   })}
 </script>
+
+
+        <script type="application/ld+json">
+  {JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: "https://www.aegiscoworking.ae/"
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Virtual Office",
+        item: "https://www.aegiscoworking.ae/virtual-office"
+      }
+    ]
+  })}
+</script>
       </Helmet>
 
       <Navbar />
