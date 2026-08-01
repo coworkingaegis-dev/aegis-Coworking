1a8932889f067802c9ee61a0b0739518c45b03a3
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-01 20:02:45 +0500

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,17 @@
+import './posthog.js'
 import { StrictMode } from 'react'
 import { createRoot, hydrateRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 if ('scrollRestoration' in window.history) {
   window.history.scrollRestoration = 'manual'
 }
-
 const rootElement = document.getElementById('root')
-
 const app = (
   <StrictMode>
     <App />
   </StrictMode>
 )
-
 if (rootElement.hasChildNodes()) {
   hydrateRoot(rootElement, app)
 } else {
