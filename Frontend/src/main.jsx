d389235b62d21ebc2f3ea607f5a658c71f51ff3a
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-08 14:35:51 +0500

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,56 @@
+// import './posthog.js'
+// import { StrictMode } from 'react'
+// import { createRoot, hydrateRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// if ('scrollRestoration' in window.history) {
+//   window.history.scrollRestoration = 'manual'
+// }
+// const rootElement = document.getElementById('root')
+// const app = (
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// )
+// if (rootElement.hasChildNodes()) {
+//   hydrateRoot(rootElement, app)
+// } else {
+//   createRoot(rootElement).render(app)
+// }
+
+
+
+
+
+
+
+
+
+
+
 import './posthog.js'
 import { StrictMode } from 'react'
 import { createRoot, hydrateRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
+
 if ('scrollRestoration' in window.history) {
   window.history.scrollRestoration = 'manual'
 }
+
 const rootElement = document.getElementById('root')
 const app = (
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 )
+
 if (rootElement.hasChildNodes()) {
   hydrateRoot(rootElement, app)
 } else {
