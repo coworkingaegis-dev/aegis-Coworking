6628d54ceaac7f0f4aa3a662eabd3d8157940e79
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-26 09:24:36 +0500

    Update BusinessPresence.jsx

--- Frontend/src/components/BusinessPresence.jsx
+++ Frontend/src/components/BusinessPresence.jsx
@@ -10,27 +10,22 @@ function BusinessPresence() {
         ADGM business address at Addax Tower, Al Reem Island, and everything you need to
         operate with credibility from day one.
       </p>
-
       <div className="bp-columns">
         <div className="bp-column">
           <div className="bp-column-icon">🏢</div>
-          <h4>WHAT'S INCLUDED</h4>
+          <h4>WHAT YOU GET</h4>
           <ul>
-            <li>Registered ADGM address at Addax Tower, Al Reem Island</li>
-            <li>Use of the address for ADGM company registration and licensing</li>
-            <li>Professional mail and package handling from ADGM authorities</li>
-            <li>Dedicated UAE phone number with live call answering</li>
+            <li>Registered ADGM business address for company registration & licensing</li>
+            <li>Professional mail handling plus a dedicated UAE phone line</li>
             <li>On-demand access to meeting rooms and the business lounge</li>
           </ul>
         </div>
         <div className="bp-column">
           <div className="bp-column-icon">📋</div>
-          <h4>ONGOING SUPPORT</h4>
+          <h4>HOW WE SUPPORT YOU</h4>
           <ul>
-            <li>Mail receiving, sorting, scanning and forwarding worldwide</li>
-            <li>Live call answering in your company name during business hours</li>
-            <li>Company name listed on the building directory</li>
-            <li>Assistance with ADGM licensing paperwork and renewals</li>
+            <li>Worldwide mail forwarding with live call answering in your company name</li>
+            <li>Building directory listing plus ADGM licensing & renewal support</li>
             <li>Seamless upgrade to a dedicated desk or private office on-site</li>
           </ul>
         </div>
