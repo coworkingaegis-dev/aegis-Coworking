3533723f3c666fb14d1ecf935c6277e3559e1365
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-27 06:21:02 +0500

    Update entry-server.jsx

--- Frontend/src/entry-server.jsx
+++ Frontend/src/entry-server.jsx
@@ -3,15 +3,18 @@ import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
+import { PreloadedDataContext } from './PreloadedDataContext.jsx'
 
-export function render(url) {
+export function render(url, preloadedData = null) {
   const helmetContext = {}
   const html = renderToString(
     <StrictMode>
       <HelmetProvider context={helmetContext}>
-        <StaticRouter location={url}>
-          <App />
-        </StaticRouter>
+        <PreloadedDataContext.Provider value={preloadedData}>
+          <StaticRouter location={url}>
+            <App />
+          </StaticRouter>
+        </PreloadedDataContext.Provider>
       </HelmetProvider>
     </StrictMode>
   )
