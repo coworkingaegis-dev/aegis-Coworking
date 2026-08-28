a2c8b41245754c297714aa4eeb6b081af07ec32a
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-28 07:11:27 +0500

    Update VirtualOfficeGallery.jsx

--- Frontend/src/components/VirtualOfficeGallery.jsx
+++ Frontend/src/components/VirtualOfficeGallery.jsx
@@ -3,11 +3,15 @@ import officeImage from '../assets/aegis-coworking-virtual-office-ADGM.jpg'
 function VirtualOfficeGallery() {
   return (
     <section className="vo-gallery-single">
-      <img
-        src={officeImage}
-        alt="Virtual office at Aegis Coworking in ADGM Abu Dhabi"
-        className="vo-gallery-single-img"
-      />
+     <img
+  src={officeImage}
+  alt="Virtual office at Aegis Coworking in ADGM Abu Dhabi"
+  className="vo-gallery-single-img"
+  width="1600"
+  height="1066"
+  loading="lazy"
+  decoding="async"
+/>
     </section>
   )
 }
