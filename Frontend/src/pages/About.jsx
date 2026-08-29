65dc8f1c0140df63b4b7b000e6703842788166e0
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-08-29 16:35:22 +0500

    Update About.jsx

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -169,11 +169,11 @@ function About() {
               </tr>
               <tr>
                 <td className="vo-plans-name" data-label="Workspace">Virtual Office</td>
-                <td className="vo-plans-price" data-label="Starting Price">AED 3,500/year</td>
+                <td className="vo-plans-price" data-label="Starting Price">AED 292/month</td>
               </tr>
               <tr>
                 <td className="vo-plans-name" data-label="Workspace">Private Office</td>
-                <td className="vo-plans-price" data-label="Starting Price">AED 4,500/month</td>
+                <td className="vo-plans-price" data-label="Starting Price">AED 4500/month</td>
               </tr>
             </tbody>
           </table>
