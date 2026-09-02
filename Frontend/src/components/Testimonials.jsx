9f222dbfc8f1a1227212b6fefa838d586f1e4b23
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-02 07:16:22 +0500

    Update Testimonials.jsx

--- Frontend/src/components/Testimonials.jsx
+++ Frontend/src/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const testimonials = [
     quote: "I was looking for a virtual office in ADGM with a professional business address. Aegis Coworking made the process straightforward, and the team was very helpful from the start.",
     name: "George Ann.",
     role: " Tech Startup Founder",
-    initials: "HA"
+    initials: "GA"
   },
 
 
@@ -19,35 +19,35 @@ const testimonials = [
     quote: " The coworking day pass is a convenient way to use a professional workspace. The daily coworking pass in just AED 100 per day is a great option for freelancers and remote workers.",
     name: "Simdhi Chakbotri.",
     role: " Software Engineer ",
-    initials: "HA"
+    initials: "SC"
   },
 
 {
     quote: "I was specifically looking for the cheapest coworking space in ADGM and wanted a privacy environment rather than just a desk. Aegis offered a good balance of price, location, and facilities.",
-    name: "Hossam Ali ",
+    name: "Jackob Smith. ",
     role: " Software analyst ",
-    initials: "HA"
+    initials: "JS"
   },
 
 {
     quote: "For businesses looking for a low cost office in ADGM, Aegis provides flexible office space and a professional seating. The team made the setup process very easy.",
     name: "Claire D.",
     role: " Business Analyst",
-    initials: "HA"
+    initials: "CD"
   },
 
 {
     quote: "I was comparing the best coworking space in ADGM and found Aegis to be a very practical choice. The workspace feels professional while keeping costs affordable",
     name: "Madona Paul",
     role: " Entrepreneur",
-    initials: "HA"
+    initials: "MD"
   },
 
 {
     quote: "Aegis Coworking is a convenient Workspace in Abu Dhabi for startups and growing companies. The flexible workspace options , meeting room and Hot Desk helped us avoid the commitment of a traditional office.",
     name: "Passimore H.",
     role: " Consultant ",
-    initials: "HA"
+    initials: "PH"
   },
 ]
 
