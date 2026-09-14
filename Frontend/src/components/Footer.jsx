b495b05c21c1c0cc4093754df8b1e85ac2b3d0e4
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-14 09:05:33 +0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -26,6 +26,12 @@ function Footer() {
               <path d="M11.5 18V10H14V11.2C14.5 10.4 15.5 9.7 17 9.7C19 9.7 20 11 20 13.3V18H17.5V13.7C17.5 12.5 17 11.8 16 11.8C15 11.8 14.3 12.5 14.3 13.7V18H11.5Z" fill="currentColor"/>
             </svg>
           </a>
+          <a href="https://www.facebook.com/aegis.coworking" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="2" y="2" width="20" height="20" rx="5" stroke="currentColor" strokeWidth="2"/>
+      <path d="M14.5 8.5H13C12.5 8.5 12 9 12 9.5V11H14.5L14 13.5H12V19H9.5V13.5H8V11H9.5V9.2C9.5 7.4 10.8 6 12.7 6H14.5V8.5Z" fill="currentColor"/>
+    </svg>
+  </a>
         </div>
       </div>
       <div className="footer-col">
