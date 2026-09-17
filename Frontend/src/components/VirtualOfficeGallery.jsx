e547a8fed5308a675f828da90ec4f412d007b4e7
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-17 10:22:17 +0500

    Update VirtualOfficeGallery.jsx

--- Frontend/src/components/VirtualOfficeGallery.jsx
+++ Frontend/src/components/VirtualOfficeGallery.jsx
@@ -1,4 +1,4 @@
-import officeImage from '../assets/aegis-coworking-virtual-office-ADGM.webp'
+import officeImage from '../assets/aegis-coworking-virtual-office-ADGM-abu-dhabi.webp'
 
 function VirtualOfficeGallery() {
   return (
