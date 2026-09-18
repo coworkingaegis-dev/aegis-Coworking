bb8f2afb2967c54987344719c88f0e5012870291
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-18 17:16:14 +0500

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -207,7 +207,7 @@ const toggleWhatWeOffer = () => {
                    <li><Link to="/private-office" onClick={closeAll}>Private Office</Link></li>
          
             <li><Link to="/meeting-room" onClick={closeAll}>Meeting Room</Link></li>
-            {/* <li><Link to="/presentation-room" onClick={closeAll}>Presentation Room</Link></li> */}
+           
             <li><Link to="/day-pass" onClick={closeAll}>Day Pass</Link></li>
           </ul>
         </li>
