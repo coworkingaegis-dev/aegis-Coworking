97094ad31bfb361ac4fb8cb55c975acc2a11916d
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-23 16:20:13 +0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
       "priceRange": "AED 100 – AED 4,500",
       "address": {
         "@type": "PostalAddress",
-      "streetAddress": "Office # 3812 Addax Tower Al Reem Island RT3",
+      "streetAddress": "Addax Tower, 3812, Al Reem Island, RT3",
 "addressLocality": "Abu Dhabi",
 "addressRegion": "Abu Dhabi",
 "addressCountry": "AE"
