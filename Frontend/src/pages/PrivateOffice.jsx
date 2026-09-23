4f4c5f551defb5a3cac1fdc7be20f75eb51b3149
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-23 15:25:42 +0500

    Update PrivateOffice.jsx

--- Frontend/src/pages/PrivateOffice.jsx
+++ Frontend/src/pages/PrivateOffice.jsx
@@ -157,7 +157,8 @@ function PrivateOffice() {
            from <strong>AED 4,500 / month</strong>, with the privacy of a
           traditional office and the flexibility of a modern business centre.
           Aegis Coworking offers private office at best price and is located at at Addax Tower, Office 3812, Al Reem Island, in the heart
-          of ADGM. 
+          of ADGM. {' '}
+                   <Link to="/pricing" className="inline-link">Promotional Offers</Link>.
         </p>
 
         <div className="bp-columns bp-columns-3">
