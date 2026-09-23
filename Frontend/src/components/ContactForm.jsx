5e8fb12f2fe7c288cf44a261224431b4d956af67
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-23 14:48:18 +0500

    Update ContactForm.jsx

--- Frontend/src/components/ContactForm.jsx
+++ Frontend/src/components/ContactForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
+
 
 function ContactForm() {
   const [formData, setFormData] = useState({
