317da39ef2b4c1c66719cfb2727e906496bc363e
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-24 05:15:50 +0500

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -217,16 +217,17 @@ const toggleWhatWeOffer = () => {
         <li><Link to="/about" onClick={closeAll}>About</Link></li>
         
         <li><Link to="/contact" onClick={closeAll}>Contact Us</Link></li>
+         <li className="nav-mobile-phone">
+  <a href="tel:+971503926316" onClick={closeAll}>
+  +971 50 392 6316
+  </a>
+</li>
         <li className="nav-mobile-cta">
           <Link to="/contact" onClick={closeAll}>
             <button className="btn-primary">REQUEST QUOTE</button>
           </Link>
         </li>
-        <li className="nav-mobile-phone">
-  <a href="tel:+971503926316" onClick={closeAll}>
-  +971 50 392 6316
-  </a>
-</li>
+       
       </ul>
      <div className="nav-desktop-cta nav-desktop-actions">
   <a href="tel:+971503926316" className="nav-phone">
