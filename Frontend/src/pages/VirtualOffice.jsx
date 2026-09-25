1887edc29c07ae7b79833150286a40024ee8eb5a
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-25 16:43:36 +0500

    Update VirtualOffice.jsx

--- Frontend/src/pages/VirtualOffice.jsx
+++ Frontend/src/pages/VirtualOffice.jsx
@@ -145,7 +145,7 @@ function VirtualOffice() {
 
       <Navbar />
       <PageHero
-       title="VIRTUAL OFFICE IN ABU DHABI – ADGM ADDRESS"
+       title="VIRTUAL OFFICE IN ABU DHABI"
         subtitle="VIRTUAL BUSINESS ADDRESS | ADGM MAIL HANDLING  | PHONE SUPPORT"
       />
       <VirtualOfficeGallery />
