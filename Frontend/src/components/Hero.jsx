521ab2ef77e063a4f81a978c330a3c5b470dd74d
coworkingaegis-dev <coworkingaegis@gmail.com>
2026-09-25 14:45:41 +0500

    Update Hero.jsx

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -54,7 +54,7 @@ function Hero() {
         
        <h1>
   COWORKING&nbsp;SPACE <br className="hero-line-break" />
-  IN&nbsp;ABU&nbsp;DHABI
+   IN&nbsp;ADGM, ABU&nbsp;DHABI
 </h1>
         <p>
          Aegis Coworking is a business centre in ADGM, offers affordable hot desk, dedicated desk, private office, virtual office, meeting room and day pass in Abu Dhabi Global Market, with plans starting from AED 1,000.
